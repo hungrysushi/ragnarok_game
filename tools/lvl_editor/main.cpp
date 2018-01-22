d7a140e1a143318b1ae7b0bc6b4156f48b106cd3
alex.freij <alex.freij@bitbucket.com>
2018-01-22 10:30:50 -0500

    notes added to level-editor files, beginning to structure FS

---
+++ tools/lvl_editor/main.cpp
@@ -0,0 +1,20 @@
+//Where the level editor is going to be called from for initalization
+/*NOTE: for renderer, different methods would have to be constructed to render different things
+    1. World renderer for background, obstacles
+    2. Render text/thought bubbles upon interaction with environment
+    3. sprite sheet parser/selector based off of input from user
+*/
+/* NOTE: for the level editor, the initial methods to be worked on are:
+    1. creating object with total dimensions that can vary (i.e: spawn level with total x and y coordinates)
+    2. returning location in the world
+    3. adding in entities that are previously defined
+    4. parsing layout in pre-determined manner for grid layout  
+    5. returning grid block size
+    6. vector resolution and aspect ratios to tell renderer to render with different sizes
+*/
+// NOTE: files in the obstacles directory will include generic geometric shapes that can be added in to world layout
+// NOTE: files in npc directory will include attributes to different npc that are called when needed
+
+int main() {
+    return 0;
+} 
