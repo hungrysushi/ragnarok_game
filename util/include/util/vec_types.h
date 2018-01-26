528a2a20297bc86f307106914bf15e7dea53db09
Ashwin Chandrasekar <achandr5@ncsu.edu>
2018-01-26 14:30:03 -0500

    Change the vectors to templated structs

--- util/include/util/vec_types.h
+++ util/include/util/vec_types.h
@@ -1,34 +1,37 @@
 #pragma once
 
+template <typename T>
 struct vec2 {
-        float x = 0.0;
-        float y = 0.0;
+        T x = 0.0;
+        T y = 0.0;
 
-        bool operator==(const vec2& rhs) const {
+        bool operator==(const vec2<T>& rhs) const {
                 return  (this->x == rhs.x) &&
                         (this->y == rhs.y);
         }
 };
 
+template <typename T>
 struct vec3 {
-        float x = 0.0;
-        float y = 0.0;
-        float z = 0.0;
+        T x = 0.0;
+        T y = 0.0;
+        T z = 0.0;
 
-        bool operator==(const vec3& rhs) const {
+        bool operator==(const vec3<T>& rhs) const {
                 return  (this->x == rhs.x) &&
                         (this->y == rhs.y) &&
                         (this->z == rhs.z);
         }
 };
 
+template <typename T>
 struct vec4 {
-        float x = 0.0;
-        float y = 0.0;
-        float z = 0.0;
-        float w = 0.0;
+        T x = 0.0;
+        T y = 0.0;
+        T z = 0.0;
+        T w = 0.0;
 
-        bool operator==(const vec4& rhs) const {
+        bool operator==(const vec4<T>& rhs) const {
                 return  (this->x == rhs.x) &&
                         (this->y == rhs.y) &&
                         (this->z == rhs.z) &&
