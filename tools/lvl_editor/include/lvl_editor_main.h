dbe3c022dee7c4ce1fd6c44f611ad6fc0936e842
alex.freij <alex.freij@bitbucket.com>
2018-01-28 19:37:52 -0500

    reorganized filesystem and added in stubs in lvl_editor_main.h file

---
+++ tools/lvl_editor/include/lvl_editor_main.h
@@ -0,0 +1,18 @@
+namespace Ragnarok {
+
+    class LevelEdit {
+
+    public:
+    // constructor and deconstructor
+        LevelEdit();
+        ~LevelEdit();
+
+    // function prototypes
+        void levelDimensions(float x, float y);
+        void levelTag();
+        void worldLocation();
+        void addEntities();
+
+    }
+
+} //namespace Ragnarok
---
+++ tools/lvl_editor/test/test_renderer.cpp
@@ -0,0 +1,5 @@
+// This file contains a dummy renderer to test different method calls to the level editor
+
+int main() {
+    return 0;
+}
